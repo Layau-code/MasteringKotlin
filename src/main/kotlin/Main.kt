dcae20c53dfdaffc6f4134f6d8897d6d22842a18
赖尧 <yujitang_2006@qq.com>
2025-10-17 17:07:35 +0800

    day05，拓展函数

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,18 @@
 package com.layor
 
     fun main(args: Array<String>){
-        val name1 = "layor"
-        val greet ="hi"
-        GreetUtil().greet(name1)
-        GreetUtil().greet(name=name1,greet)
-
-        3 times "hello"
-
+        val address ="1891@.com"
+        val isValid = address.isValidEmail()
+        println(isValid)
+        val numbers = listOf(1,2,3,4,5)
+        val sum = numbers.sum()
+        println(sum)
     }
-infix fun Int.times(str:String){
-   val result = str.repeat( this)
-    println(result)
+//扩展函数
+fun String.isValidEmail(): Boolean {
+    return Regex("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+").matches(this)
 }
-
-
-    class GreetUtil{
-        fun greet(name: String?,greet : String = "hello"){
-            println("$greet!$name")
-        }
-    }
+//扩展List<Int>
+fun List<Int>.sum(): Int {
+    return this.fold(0) { sum, i -> sum + i }
+}
